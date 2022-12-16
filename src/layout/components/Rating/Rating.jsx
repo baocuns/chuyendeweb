a52dfd1184879ae46295a7a806508fae67f4305b
Bùi Duy Khánh <81368707+buiduykhanh030401@users.noreply.github.com>
2022-12-16 23:32:22 +0700

    save cart update

---
+++ src/layout/components/Rating/Rating.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Rating() {
+    return (
+        <div className="rating">
+            <input type="radio" name="rating-1" className="mask mask-star" />
+            <input type="radio" name="rating-1" className="mask mask-star" />
+            <input type="radio" name="rating-1" className="mask mask-star" />
+            <input type="radio" name="rating-1" className="mask mask-star" />
+            <input type="radio" name="rating-1" className="mask mask-star" />
+        </div>
+    )
+}
+
+export default Rating
