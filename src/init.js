67143f159907aea161d9e824d6da68600799f90c
Bùi Duy Khánh <81368707+buiduykhanh030401@users.noreply.github.com>
2022-12-24 15:49:09 +0700

    finish rating function

--- src/init.js
+++ src/init.js
@@ -2,5 +2,7 @@ const API_HOST = 'https://api.travels.games'
 // const API_HOST = 'http://localhost'
 const DETAIL_TOUR_ROUTE = `${API_HOST}/api/v1/tour/show/details`
 const ADD_TO_CART_ROUTE = `${API_HOST}/api/v1/cart/store/`
+const sendRatingRoute = `${API_HOST}/api/v1/rating/store`;
+const showAllRatingRoute = `${API_HOST}/api/v1/rating/show`;
 
-export { API_HOST, DETAIL_TOUR_ROUTE, ADD_TO_CART_ROUTE }
+export { API_HOST, DETAIL_TOUR_ROUTE, ADD_TO_CART_ROUTE, sendRatingRoute, showAllRatingRoute }
--- src/layout/components/Rating/Rating.jsx
+++ src/layout/components/Rating/Rating.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Rating() {
     return (
         <div className="rating">
-            <input type="radio" name="rating-1" className="mask mask-star" />
-            <input type="radio" name="rating-1" className="mask mask-star" />
-            <input type="radio" name="rating-1" className="mask mask-star" />
-            <input type="radio" name="rating-1" className="mask mask-star" />
-            <input type="radio" name="rating-1" className="mask mask-star" />
+            <input type="radio" name="rating-1" className="mask mask-star bg-yellow-300" value={1} />
+            <input type="radio" name="rating-1" className="mask mask-star bg-yellow-300" value={2} />
+            <input type="radio" name="rating-1" className="mask mask-star bg-yellow-300" value={3} />
+            <input type="radio" name="rating-1" className="mask mask-star bg-yellow-300" value={4} />
+            <input type="radio" name="rating-1" className="mask mask-star bg-yellow-300" value={5} />
         </div>
     )
 }
